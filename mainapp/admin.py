5e6d72f1af5c9cdd512aba56b3176e2f690bfc1f
Danil <vinniopo@mail.ru>
2021-11-23 17:22:41 +0700

    ДЗ урок 3, Мосин Д.О.

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from mainapp.models import Product_Category, Product
+
 # Register your models here.
+admin.site.register(Product_Category)
+admin.site.register(Product)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from  mainapp.models import Product
 
 # Create your views here.
 
@@ -13,25 +14,6 @@ def index(request):
 def products(request):
     context = {
         'title': 'товары',
-        'products': [
-            {
-            'name': 'Стул',
-            'image': 'chair_1.jpg',
-            'description': 'Отличный стул',
-            'price': 3050
-            },
-            {
-                'name': 'Белый стул',
-                'image': 'product-21.jpg',
-                'description': 'Элитный стул',
-                'price': 7250
-            },
-            {
-                'name': 'Часы настенные',
-                'image': 'product-4-sm.jpg',
-                'description': 'Очень красивые',
-                'price': 2100
-            },
-        ]
+        'products': Product.objects.all()
     }
     return render(request, 'mainapp/products.html', context)
