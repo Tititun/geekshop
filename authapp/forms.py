bbe6525eac3c87677ecb12ed6f21ba9b77e564f3
Danil <vinniopo@mail.ru>
2021-11-24 15:07:20 +0700

    ДЗ урок 4, Мосин Д.О.

---
+++ authapp/forms.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from authapp.models import User
+from django.core.exceptions import ValidationError
+import re
+
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Введите имя пользователя'
+        self.fields['password'].widget.attrs['placeholder'] = 'Введите пароль'
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+
+class UserRegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Введите имя пользователя'
+        self.fields['email'].widget.attrs['placeholder'] = 'Введите адрес эл. почты'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Введите имя'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Введите фамилию'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Введите пароль'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Повторите пароль'
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+    def clean_first_name(self):
+        data = self.cleaned_data['first_name']
+        if re.search(r'\d', data):
+            raise ValidationError('Имя не должно содержать цифры')
+
+        return data
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name',
+                  'last_name', 'password1', 'password2')
--- authapp/urls.py
+++ authapp/urls.py
@@ -14,17 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from authapp.views import login, register, logout
 
-from mainapp.views import index, products
 
+app_name='authapp'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='main'),
-    path('products/', include('mainapp.urls', namespace='mainapp')),
-    path('users/', include('authapp.urls', namespace='authapp')),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
