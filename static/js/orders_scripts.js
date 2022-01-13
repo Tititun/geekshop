d1ad600f74e5ce168e20e70a64e92be49bf52eb3
Danil <vinniopo@mail.ru>
2022-01-13 12:25:45 +0700

    cache_property and js fixes

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -70,4 +70,30 @@ window.onload = function () {
         delta_quantity = -quantity_arr[orderitem_num];
         orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
     }
+    console.log('here')
+    $('.basket_list').on('click', 'input[type="number"]', function (e) {
+        let t_href = e.target;
+        $.ajax(
+            {
+                'url': "/baskets/edit/" + t_href.name + "/" + t_href.value,
+                'success': function (data) {
+                    $('.basket_list').html(data.result)
+                }
+            }
+        )
+        e.preventDefault()
+    })
+
+    $('.card_add_basket').on('click', 'button', function (e) {
+        let t_href = e.target.value;
+        $.ajax(
+            {
+                'url': "/baskets/add/" + t_href + "/",
+                'success': function (data) {
+                    $('.card_add_basket').html(data.result)
+                }
+            }
+        )
+        e.preventDefault()
+    })
 }
